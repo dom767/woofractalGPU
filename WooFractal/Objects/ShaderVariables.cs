872664b334a5866154adfceb04eabf32597195bd
dom767 <dominicpenfold@gmail.com>
2017-10-08 14:53:49 +0100

    Shader var support
    Added float arrays and vec3 arrays for upcoming colour modifications

--- WooFractal/Objects/ShaderVariables.cs
+++ WooFractal/Objects/ShaderVariables.cs
@@ -13,7 +13,9 @@ namespace WooFractal.Objects
         Float = 1,
         Vec3 = 2,
         Mat3 = 3,
-        Mat4 = 4
+        Mat4 = 4,
+        FloatArray = 5,
+        Vec3Array = 6
     };
 
     public class ShaderVariable
@@ -42,6 +44,18 @@ namespace WooFractal.Objects
             _Mat4Value = value;
             _Type = SVType.Mat4;
         }
+        public ShaderVariable(string name, float[] values)
+        {
+            _Name = name;
+            _FloatArrayValues = values;
+            _Type = SVType.FloatArray;
+        }
+        public ShaderVariable(string name, vec3[] values)
+        {
+            _Name = name;
+            _Vec3ArrayValues = values;
+            _Type = SVType.Vec3Array;
+        }
         public void Set(OpenGL gl, ShaderProgram shader)
         {
             switch (_Type)
@@ -57,6 +71,21 @@ namespace WooFractal.Objects
                     break;
                 case SVType.Mat4:
                     shader.SetUniformMatrix4(gl, _Name, _Mat4Value.to_array());
+                    break;
+                case SVType.FloatArray:
+                    int rte1 = shader.GetUniformLocation(gl, _Name);
+                    gl.Uniform1(rte1, _FloatArrayValues.Count(), _FloatArrayValues);
+                    break;
+                case SVType.Vec3Array:
+                    int rte2 = shader.GetUniformLocation(gl, _Name);
+                    float[] tempFloats = new float[_Vec3ArrayValues.Count() * 3];
+                    for (int i = 0; i < _Vec3ArrayValues.Count(); i++)
+                    {
+                        tempFloats[i * 3 + 0] = _Vec3ArrayValues[i].x;
+                        tempFloats[i * 3 + 1] = _Vec3ArrayValues[i].y;
+                        tempFloats[i * 3 + 2] = _Vec3ArrayValues[i].z;
+                    }
+                    gl.Uniform3(rte2, _Vec3ArrayValues.Count(), tempFloats);
                     break;
             }
         }
@@ -94,6 +123,26 @@ namespace WooFractal.Objects
                         ret += vals[15] + ")";
                         return ret;
                     }
+                case SVType.FloatArray:
+                    {
+                        string ret = "{";
+                        for (int i = 0; i < _FloatArrayValues.Count() - 1; i++)
+                        {
+                            ret += _FloatArrayValues[i] + ", ";
+                        }
+                        ret += _FloatArrayValues[_FloatArrayValues.Count()-1] + "}";
+                        return ret;
+                    }
+                case SVType.Vec3Array:
+                    {
+                        string ret = "vec3[](";
+                        for (int i = 0; i < _Vec3ArrayValues.Count() - 1; i++)
+                        {
+                            ret += "vec3("+_Vec3ArrayValues[i].x+", "+ _Vec3ArrayValues[i].y+", "+_Vec3ArrayValues[i].z + "), ";
+                        }
+                        ret += "vec3("+_Vec3ArrayValues[_Vec3ArrayValues.Count() - 1].x+", "+ _Vec3ArrayValues[_Vec3ArrayValues.Count() - 1].y+", "+_Vec3ArrayValues[_Vec3ArrayValues.Count() - 1].z + "))";
+                        return ret;
+                    }
             }
             return "";
         }
@@ -123,6 +172,8 @@ namespace WooFractal.Objects
         vec3 _Vec3Value;
         mat3 _Mat3Value;
         mat4 _Mat4Value;
+        float[] _FloatArrayValues;
+        vec3[] _Vec3ArrayValues;
     };
 
     public class ShaderVariables
@@ -154,6 +205,14 @@ namespace WooFractal.Objects
         public void Add(string name, mat4 value)
         {
             _ShaderVariable.Add(new ShaderVariable(name, value));
+        }
+        public void Add(string name, float[] values)
+        {
+            _ShaderVariable.Add(new ShaderVariable(name, values));
+        }
+        public void Add(string name, vec3[] values)
+        {
+            _ShaderVariable.Add(new ShaderVariable(name, values));
         }
 
         public void Set(OpenGL gl, ShaderProgram shader)
